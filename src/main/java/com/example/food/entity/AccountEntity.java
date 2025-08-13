f16aaf51875b64d972c66b6adb1407bf11d6ba0d
linhdh52 <liq.97tn@gmail.com>
2025-08-13 17:11:48 +0700

    up db

---
+++ src/main/java/com/example/food/entity/AccountEntity.java
@@ -0,0 +1,44 @@
+package com.example.food.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "account")
+public class AccountEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "device_seq")
+    @SequenceGenerator(name = "device_seq",sequenceName = "device_seq_name",allocationSize = 1)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "number")
+    private Long number;
+
+    @Column(name = "sex")
+    private boolean sex;
+
+    @Column(name = "birthday")
+    private Date birthday;
+
+    @Column(name = "time_create_user")
+    private String timeCreateUser;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "accumulate_points")
+    private Long accumulatePoints;
+
+    @Column(name = "level")
+    private Long level;
+
+    @Column(name = "purchase_history")
+    private Date purchaseHistory;
+}
