d584a9786c75c9ce74dedb7c5184dc190442cd07
linhdh52 <liq.97tn@gmail.com>
2025-08-21 11:04:07 +0700

    account

--- src/main/java/com/example/food/entity/AccountEntity.java
+++ src/main/java/com/example/food/entity/AccountEntity.java
@@ -12,14 +12,11 @@ public class AccountEntity {
     @Column(name = "user_id")
     private Long id;
 
-    @Column(name = "name")
-    private String name;
+    @Column(name = "number")
+    private Long number;
 
     @Column(name = "password")
     private String password;
-
-    @Column(name = "number")
-    private Long number;
 
     @Column(name = "sex")
     private boolean sex;
@@ -41,4 +38,87 @@ public class AccountEntity {
 
     @Column(name = "purchase_history")
     private Date purchaseHistory;
+
+    public AccountEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getNumber() {
+        return number;
+    }
+
+    public void setNumber(Long number) {
+        this.number = number;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getTimeCreateUser() {
+        return timeCreateUser;
+    }
+
+    public void setTimeCreateUser(String timeCreateUser) {
+        this.timeCreateUser = timeCreateUser;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getAccumulatePoints() {
+        return accumulatePoints;
+    }
+
+    public void setAccumulatePoints(Long accumulatePoints) {
+        this.accumulatePoints = accumulatePoints;
+    }
+
+    public Long getLevel() {
+        return level;
+    }
+
+    public void setLevel(Long level) {
+        this.level = level;
+    }
+
+    public Date getPurchaseHistory() {
+        return purchaseHistory;
+    }
+
+    public void setPurchaseHistory(Date purchaseHistory) {
+        this.purchaseHistory = purchaseHistory;
+    }
 }
